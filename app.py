b15dd1fcdd38241f17d3c1299ee9cf95568e9788
Diego Rocha <diego@diegorocha.com.br>
2016-06-22 00:18:19 -0300

    Adaptado para heroku

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
+from decouple import config
+from diegor_me import get_url_from_alias
 from flask import Flask, render_template, redirect, abort
-import diegor_me
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return handle_alias()
 
+
 @app.route("/<alias>")
 def alias_url(alias):
     return handle_alias(alias)
+
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template("404.htm"), 404
 
+
 def handle_alias(alias=''):
-    url = diegor_me.get_url_from_alias(alias)
+    url = get_url_from_alias(alias)
     if url:
-       return redirect(url, code=302)
+        return redirect(url, code=302)
     else:
-       abort(404)
+        abort(404)
 
 if __name__ == '__main__':
-    app.run()
+    debug = config('debug', default=False, cast=bool)
+    app.run(debug=debug)
--- diegor_me.py
+++ diegor_me.py
@@ -1,9 +1,15 @@
 import boto3
+from decouple import config
+
 
 def get_url_from_alias(alias=''):
     if alias == '':
         alias = '/'
-    dynamodb = boto3.resource('dynamodb')
+
+    dynamodb = boto3.resource('dynamodb',
+                              aws_access_key_id=config('aws_access_key_id'),
+                              aws_secret_access_key=config('aws_secret_access_key'),
+                              region_name=config('region'))
     table = dynamodb.Table('diegor_me')
     search = {'alias': alias}
     response = table.get_item(Key=search)
